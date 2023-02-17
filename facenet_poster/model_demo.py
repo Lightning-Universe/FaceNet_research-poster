18458eb1b51675aac94124143419f2e8c7f024b8
Aniket Maurya <theaniketmaurya@gmail.com>
2023-02-17 12:41:17 +0530

    fix import

--- facenet_poster/model_demo.py
+++ facenet_poster/model_demo.py
@@ -8,7 +8,7 @@ from lightning.app.components.serve import ServeGradio
 from PIL import Image
 from rich.logging import RichHandler
 
-from research_app.facenet_demo import FaceNetDemo
+from facenet_demo import FaceNetDemo
 
 FORMAT = "%(message)s"
 logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
